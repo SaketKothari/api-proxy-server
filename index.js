e83a0807e62878837d4460ff8233c90e35317afc
Saket Kothari <saketkothari722@gmail.com>
2022-12-18 10:19:39 +0530

    Rate Limiting on the API

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMS: 10 * 60 * 1000, // 10 mins
+  max: 20,
+});
+app.use(limiter);
+app.set('trust proxy', 1);
 
 // Routes
 app.use('/api', require('./routes'));
