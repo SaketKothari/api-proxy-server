a5918055de752558a3ab5d24d24dc50aacb53ca2
Saket Kothari <saketkothari722@gmail.com>
2022-12-18 10:32:59 +0530

    Client Side

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const limiter = rateLimit({
 app.use(limiter);
 app.set('trust proxy', 1);
 
+// Set static folder
+app.use(express.static('public'));
+
 // Routes
 app.use('/api', require('./routes'));
 
